5ce97d3cb9207cd3d071a3b2341a651155b32231
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-07-19 17:21:21 -0400

    browser tests working
    - metamask does not load when loading file from disk.  web3 is loaded from
    disk instead of metamask.
    - loading html test runner via nginx fails to load script files.

--- test/browser/browser.test.js
+++ test/browser/browser.test.js
@@ -8,18 +8,19 @@ describe('Test browser code', () => {
   const assert = chai.assert
 
   before(function () {
-  // runs before all tests in this block
+    // runs before all tests in this block
+    App.init()
   })
 
   describe('Test App.js', () => {
     it('App.js should be present', () => {
-      assert.exists(App, 'App is null nor undefined')
+      assert.exists(App, 'App is null or undefined')
     })
   })
 
-  describe('Detect Metamask', () => {
-    it('should return true when Metamask is detected', () => {
-      assert.isTrue(false, 'test not yet implemented')
+  describe('Detect web3Provider', () => {
+    it('should be available', () => {
+      assert.isOk(App.web3Provider, 'Provider is not available')
     })
   })
 })
