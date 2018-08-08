76a1242adab26dfcb4935b00bcc9ed72e5c207ec
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-08-08 19:28:47 -0400

    reorg browser testing

---
+++ src/js/browser.test.js
@@ -0,0 +1,27 @@
+/* global describe, it, before, App, chai */
+
+/* this file is for inbrowser testing
+ * for jquery testing here are ideas
+ * https://gist.github.com/robballou/9ee108758dc5e0e2d028
+ * */
+
+describe('Test browser code', function () {
+  const assert = chai.assert
+
+  before(function () {
+    // runs before all tests in this block
+    // App.init()
+  })
+
+  describe('App.js', function () {
+    it('should be present', function () {
+      assert.exists(App, 'App is null or undefined')
+    })
+  })
+
+  describe('web3Provider', function () {
+    it('should be available', function () {
+      assert.isOk(App.web3Provider, 'Provider is not available')
+    })
+  })
+})
--- test/browser/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-xdg-open ./browser.test.html
