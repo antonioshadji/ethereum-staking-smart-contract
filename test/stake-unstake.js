b755e1953caf0aa4440b5c4f1263b87f2760020d
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-08-15 12:27:57 -0400

    added test

--- test/stake-unstake.js
+++ test/stake-unstake.js
@@ -7,25 +7,25 @@ const StakePool = artifacts.require('StakePool')
 const StakeContract = artifacts.require('StakeContract')
 
 contract(`stake / unstake functions: ${p.basename(__filename)}`, function (accounts) {
-  let poolAddress = null
-  let stakeAddress = null
+  let pool = null
+  let stak = null
   let log = null
   before('show contract addresses', function () {
     log = fs.createWriteStream(`./test/logs/testrun.log`)
     log.write(`${(new Date()).toISOString()}\n`)
     StakeContract.deployed().then(function (instance) {
-      stakeAddress = instance.address
+      stak = instance
       log.write(`StakeContract: ${instance.address}\n`)
     })
     StakePool.deployed().then(function (instance) {
-      poolAddress = instance.address
+      pool = instance
       log.write(`StakePool: ${instance.address}\n`)
     })
   })
 
   it(`should show a zero balance in StakeContract`, function () {
     assert.equal(
-      web3.eth.getBalance(stakeAddress).valueOf(),
+      web3.eth.getBalance(stak.address).valueOf(),
       web3.toWei(0, 'ether'),
       'StakeContract account balance is not as expected'
     )
@@ -33,10 +33,22 @@ contract(`stake / unstake functions: ${p.basename(__filename)}`, function (accou
 
   it(`should show a zero balance in StakePool`, function () {
     assert.equal(
-      web3.eth.getBalance(poolAddress).valueOf(),
+      web3.eth.getBalance(pool.address).valueOf(),
       web3.toWei(0, 'ether'),
       'StakeContract account balance is not as expected'
     )
+  })
+
+  it(`should be able to check stake address with pool getter`, function () {
+    // stakeContract is name of public global variable in StakePool contract
+    // the compiler created getter, returns a Promise
+    return pool.stakeContract()
+      .then(function (val) {
+        return val
+      })
+      .then(function (a) {
+        assert.equal(a, stak.address)
+      })
   })
 
   it(`should be able to stake ether to StakeContract`, function () {
@@ -59,7 +71,7 @@ contract(`stake / unstake functions: ${p.basename(__filename)}`, function (accou
 
   it(`should show a balance of 1 ether in StakeContract`, function () {
     assert.equal(
-      web3.eth.getBalance(stakeAddress).valueOf(),
+      web3.eth.getBalance(stak.address).valueOf(),
       web3.toWei(1, 'ether'),
       'StakeContract account balance is not as expected'
     )
@@ -67,7 +79,7 @@ contract(`stake / unstake functions: ${p.basename(__filename)}`, function (accou
 
   it(`should show a zero balance in StakePool`, function () {
     assert.equal(
-      web3.eth.getBalance(poolAddress).valueOf(),
+      web3.eth.getBalance(pool.address).valueOf(),
       web3.toWei(0, 'ether'),
       'StakePool account balance is not as expected'
     )
@@ -92,7 +104,7 @@ contract(`stake / unstake functions: ${p.basename(__filename)}`, function (accou
 
   it(`should show a balance of 0 ether in StakeContract`, function () {
     assert.equal(
-      web3.eth.getBalance(stakeAddress).valueOf(),
+      web3.eth.getBalance(stak.address).valueOf(),
       web3.toWei(0, 'ether'),
       'StakeContract account balance is not as expected'
     )
@@ -100,7 +112,7 @@ contract(`stake / unstake functions: ${p.basename(__filename)}`, function (accou
 
   it(`should show a 0 balance in StakePool`, function () {
     assert.equal(
-      web3.eth.getBalance(poolAddress).valueOf(),
+      web3.eth.getBalance(pool.address).valueOf(),
       web3.toWei(0, 'ether'),
       'StakePool account balance is not as expected'
     )
