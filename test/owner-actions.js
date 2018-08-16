d361b954c4c5af1fdc035f837b94b18dfff029e2
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-08-16 10:56:58 -0400

    consolidated tests

--- test/owner-actions.js
+++ test/owner-actions.js
@@ -1,4 +1,5 @@
-/* global artifacts, contract, it, web3 */
+/* global after, assert, before, artifacts, contract, it, web3 */
+/* Truffle injects all these globals */
 // Mocha has an implied describe() block, called the “root suite”).
 
 /* all web3 are synchronous by default, AND can take an optional callback
@@ -8,11 +9,30 @@
  * Promises
  */
 const p = require('path')
-const assert = require('chai').assert
+const fs = require('fs')
 const StakePool = artifacts.require('StakePool')
-// const contracts = []
+const StakeContract = artifacts.require('StakeContract')
 
-contract(`Stakepool owner access: ${p.basename(__filename)}`, function (accounts) {
+const fn = p.basename(__filename)
+
+contract(`Stakepool owner access: ${fn}`, function (accounts) {
+  let pool = null
+  let stak = null
+  let log = null
+  before('show contract addresses', function () {
+    log = fs.createWriteStream(`./test/logs/${fn}.log`)
+    log.write(`${(new Date()).toISOString()}\n`)
+    log.write(`web3 version: ${web3.version.api}\n`)
+    StakeContract.deployed().then(function (instance) {
+      stak = instance
+      log.write(`StakeContract: ${instance.address}\n`)
+    })
+    StakePool.deployed().then(function (instance) {
+      pool = instance
+      log.write(`StakePool: ${instance.address}\n`)
+    })
+  })
+
   it('should receive ether when sent to instance address', function () {
     return StakePool.deployed().then(function (instance) {
       return web3.eth.sendTransaction(
@@ -42,5 +62,21 @@ contract(`Stakepool owner access: ${p.basename(__filename)}`, function (accounts
       let vw = obj.logs[0].args.valueWithdrawn.toNumber()
       assert.equal(vw, 0, 'logs should confirm 0 balance')
     })
+  })
+
+  it(`should be able to check stake address with pool getter`, function () {
+    // stakeContract is name of public global variable in StakePool contract
+    // the compiler created getter, returns a Promise
+    return pool.stakeContract()
+      .then(function (val) {
+        return val
+      })
+      .then(function (a) {
+        assert.equal(a, stak.address)
+      })
+  })
+
+  after('finished', function () {
+    log.end()
   })
 })
